adf71ca1a2b663b0986cb9c7f11cc02621db724b
Antoine Lauzon <anlau@users.noreply.github.com>
2019-01-22 18:57:28 -0500

    Focus search input when opening search modal
    Used ids are found in base.html.

--- cinder/js/base.js
+++ cinder/js/base.js
@@ -1,8 +1,13 @@
 $(document).ready(function() {
     hljs.initHighlightingOnLoad();
-    $('table').addClass('table table-striped table-hover');
-});
 
+    $('table').addClass('table table-striped table-hover');
+
+    var $searchModal = $('#mkdocs_search_modal');
+    $searchModal.on('shown.bs.modal', function() {
+        $searchModal.find('#mkdocs-search-query').focus();
+    });
+});
 
 $('body').scrollspy({
     target: '.bs-sidebar',
