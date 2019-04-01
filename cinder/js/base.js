8fd1726a1fd9b99073fc9b04573ff955d8aeb6db
Tim Ganter <ganter@adobe.com>
2019-04-01 13:42:42 -0700

    Add getSearchTerm(). Copied from themes/mkdocs/js/base.js

--- cinder/js/base.js
+++ cinder/js/base.js
@@ -1,3 +1,17 @@
+function getSearchTerm()
+{
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++)
+    {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == 'q')
+        {
+            return sParameterName[1];
+        }
+    }
+}
+
 $(document).ready(function() {
     hljs.initHighlightingOnLoad();
 
